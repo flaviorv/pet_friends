ccc1697c691c90792c836b0f5bee2d0130ce4d80
flaviorv <frezendevaroto@gmail.com>
2024-08-30 18:07:49 -0300

    create a messager receiver for a pub/sub messaging in the cloud. obs: the key file were removed by the gitignore

---
+++ passeador/src/main/java/com/passeador/infra/ServicoAgendadoMessage.java
@@ -0,0 +1,42 @@
+package com.passeador.infra;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.AckMode;
+import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicoAgendadoMessage {
+
+    private  static final Logger LOG = LoggerFactory.getLogger(ServicoAgendadoMessage.class);
+
+    @Bean
+    public MessageChannel inputMessageChannel() {
+        return new PublishSubscribeChannel();
+    }
+
+    @Bean
+    public PubSubInboundChannelAdapter inboundChannelAdapter(@Qualifier("inputMessageChannel") MessageChannel messageChannel, PubSubTemplate pubSubTemplate) {
+        PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "pet-friends-servico-agendado-sub");
+        adapter.setOutputChannel(messageChannel);
+        adapter.setAckMode(AckMode.MANUAL);
+        adapter.setPayloadType(String.class);
+        return adapter;
+    }
+
+    @ServiceActivator(inputChannel = "inputMessageChannel")
+    public void MessageReciever(String payload, @Header(GcpPubSubHeaders.ORIGINAL_MESSAGE)BasicAcknowledgeablePubsubMessage message) {
+        LOG.info("=====Mensagem-Recebida=====  " + payload);
+        message.ack();
+    }
+}
